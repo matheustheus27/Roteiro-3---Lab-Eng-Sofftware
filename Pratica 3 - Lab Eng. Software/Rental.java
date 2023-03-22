34582285ba6073fd4ba3567b21b480c8a57171d3
Matheus Thiago <31577877+matheustheus27@users.noreply.github.com>
2023-03-22 11:28:16 -0300

    Commit 2

--- Pratica 3 - Lab Eng. Software/Rental.java
+++ Pratica 3 - Lab Eng. Software/Rental.java
@@ -2,17 +2,17 @@ public class Rental {
 
     private Movie _movie;
     private int _daysRented;
- 
+
     public Rental(Movie movie, int daysRented) {
-       _movie = movie;
-       _daysRented = daysRented;
+        _movie = movie;
+        _daysRented = daysRented;
     }
- 
+
     public int getDaysRented() {
-       return _daysRented;
+        return _daysRented;
     }
- 
+
     public Movie getMovie() {
-       return _movie;
+        return _movie;
     }
- }
+}
